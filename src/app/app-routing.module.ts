f4a7fa0a18c9c0f44a135a09e1cf4caebed31bc2
Probe-House <bheer31@gmail.com>
2018-11-22 01:31:25 -0500

    register login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { FeaturedGamesComponent } from './featured-games/featured-games.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',   redirectTo: '/featured', pathMatch: 'full' },
+  {path:'register', component: RegisterComponent},  
+  {path:'login', component: LoginComponent},  
+  {path:'featured', component: FeaturedGamesComponent},  
+  {path:'reset-password', component: ResetPasswordComponent},  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [RegisterComponent, LoginComponent, FeaturedGamesComponent, ResetPasswordComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowseByTagBarComponent } from './browse-by-tag-bar/browse-by-tag-bar.component';
 import { TagElementComponent } from './tag-element/tag-element.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { FeaturedGamesComponent } from './featured-games/featured-games.component';
+
 
 @NgModule({
   declarations: [
@@ -15,8 +15,8 @@ import { FeaturedGamesComponent } from './featured-games/featured-games.componen
     HeaderComponent,
     BrowseByTagBarComponent,
     TagElementComponent,
-    SidebarComponent,
-    FeaturedGamesComponent
+    SidebarComponent,    
+    routingComponents,  
   ],
   imports: [
     BrowserModule,
